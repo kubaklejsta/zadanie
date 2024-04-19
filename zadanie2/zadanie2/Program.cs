0852bd72e76fe551bab75a8676f8f1ea52dd28f3
kubaklejsta <152186932+kubaklejsta@users.noreply.github.com>
2024-04-19 10:26:48 +0200

    Add files via upload

---
+++ zadanie2/zadanie2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public class AplikacjaSortowanie
+{
+    private const int ArrayLength = 10;
+
+    public static void Main(string[] args)
+    {
+        int[] numbers = new int[ArrayLength];
+        Console.WriteLine("wpisz liczby:");
+
+        for (int i = 0; i < ArrayLength; i++)
+        {
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        SortArray(numbers);
+
+        Console.WriteLine("posortowane liczby:");
+        for (int i = 0; i < ArrayLength; i++)
+        {
+            Console.WriteLine(numbers[i]);
+        }
+    }
+
+    private static void SortArray(int[] numbers)
+    {
+        for (int i = 0; i < ArrayLength - 1; i++)
+        {
+            int minIndex = i;
+            for (int j = i + 1; j < ArrayLength; j++)
+            {
+                if (numbers[j] > numbers[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+
+            if (minIndex != i)
+            {
+                Swap(numbers, i, minIndex);
+            }
+        }
+    }
+
+    private static void Swap(int[] numbers, int i, int j)
+    {
+        int temp = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = temp;
+    }
+}
---
+++ zadanie2/zadanie2/sort.cs
@@ -0,0 +1,9 @@
+﻿
+
+//rtArray(int[] numbers): Sortuje tablicę numbers w kolejności rosnącej.
+//wap(int[] numbers, int i, int j): Wymienia dwa elementy w tablicy numbers o indeksach i i j.
+
+//Algorytm sortowania przez wybieranie działa w następujący sposób:
+
+//Iteruje po tablicy, wybierając za każdym razem najmniejszy element niesposortowany i wymieniając go z pierwszym niesposortowanym elementem.
+//Proces ten jest powtarzany, aż do momentu, gdy wszystkie elementy tablicy zostaną posortowane.
